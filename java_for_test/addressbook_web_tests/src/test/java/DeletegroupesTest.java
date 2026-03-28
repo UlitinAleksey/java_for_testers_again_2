e0bd4053a4f32015ffe59f9452a18a5c4cfee349
UlitinAleksey <tjlonus@icloud.com>
2026-03-28 21:40:16 +0400

    Создал тесты по созданию/удалению контакта

--- java_for_test/addressbook_web_tests/src/test/java/DeletegroupesTest.java
+++ java_for_test/addressbook_web_tests/src/test/java/DeletegroupesTest.java
@@ -1,67 +1,20 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import model.GroupData;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class DeletegroupesTest {
-    private WebDriver driver;
+public class DeletegroupesTest extends TestBase {
 
-    @BeforeEach
-    public void setUp() {
-        driver = new FirefoxDriver();
-        driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
 
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.findElement(By.linkText("Logout")).click();
-        driver.quit();
-    }
 
     @Test
     public void canRemoveGroup() {
-        if (! isElementPresent(By.name("new"))){
-            driver.findElement(By.linkText("groups")).click();
-        }
-        if (! isElementPresent(By.name("selected[]"))){
-            driver.findElement(By.name("new")).click();
-            driver.findElement(By.name("group_name")).click();
-            driver.findElement(By.name("group_name")).sendKeys("test gr 2");
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).sendKeys("test head 2");
-            driver.findElement(By.name("group_footer")).click();
-            driver.findElement(By.name("group_footer")).sendKeys("test foot 2");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.id("header")).click();
-            driver.findElement(By.linkText("groups")).click();
-            driver.findElement(By.id("footer")).click();
+        openGroupsPage();
+        if (!isGroupPresent()){
+            createGroup(new GroupData("test gr 2", "test head 2", "test foot 2"));
         }
 
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
+        removeGroup();
 
 
     }
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        }catch (NoSuchElementException exception){
-            return false;
-        }
 
-    }
 }
