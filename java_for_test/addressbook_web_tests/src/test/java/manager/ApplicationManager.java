a4a6d400dad821f1642733793e4b7b381646296e
UlitinAleksey <tjlonus@icloud.com>
2026-04-19 23:33:00 +0400

    Задание 13

--- java_for_test/addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ java_for_test/addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
     public static WebDriver driver;
 
@@ -10,8 +12,10 @@ public class ApplicationManager {
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
+    private Properties properties;
 
-    public void init(String browser) {
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("firefox".equals(browser)) {
                 driver = new FirefoxDriver();
@@ -22,9 +26,9 @@ public class ApplicationManager {
             }
 
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1936, 1056));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
